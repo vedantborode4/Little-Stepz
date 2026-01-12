79321e046126fd78b7b1755cef846d8ab0ff78d3
Vedant <borodevedant4@gmail.com>
2026-01-12 19:26:34 +0530

    feat(db): reinitialize Prisma and add User model and checked connection

--- apps/backend/src/controllers/auth.controllers.ts
+++ apps/backend/src/controllers/auth.controllers.ts
@@ -1,6 +1,16 @@
 import { Request, Response } from "express";
+import { prisma } from "@repo/db/client";
 
 export async function  signupController (req : Request, res: Response) {
+    await prisma.user.create({
+    data: {
+        firstName: "John",
+        email: "john@example.com",
+        password: "hashedPassword",
+        role: "USER",
+    },
+    });
+
     return (res.send("Random return from signup"))
 }
 
