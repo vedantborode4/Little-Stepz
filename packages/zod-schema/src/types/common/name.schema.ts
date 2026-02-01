4bff98a6123d68bcd68761e6892745249f3a033e
Vedant <borodevedant4@gmail.com>
2026-02-02 04:24:30 +0530

    fix(zod): make nameSchema string from object

--- packages/zod-schema/src/types/common/name.schema.ts
+++ packages/zod-schema/src/types/common/name.schema.ts
@@ -1,7 +1,5 @@
 import z from "zod";
 
-export const nameSchema = z.object({
-  name: z.string()
+export const nameSchema = z.string()
     .min(2, "Name must be at least 2 characters")
     .max(50, "Name must be at most 50 characters")
-})
