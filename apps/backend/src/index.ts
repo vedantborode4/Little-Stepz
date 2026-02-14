9eba402bbfc2b947619e17d897932fca3755ff46
Vedant <borodevedant4@gmail.com>
2026-02-14 23:54:23 +0530

    feat(CORS): add CORS for frontend origin

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -3,11 +3,18 @@ import "dotenv/config"
 import { appRouter } from "./routes";
 import cookieParser from "cookie-parser"
 import { errorHandler } from "./middlewares/errorHandler.middleware";
+import cors from "cors";
 
 const app = express();
+const ORIGIN = process.env.ORIGIN || "http://localhost:3000";
 
 app.use(express.json())
 app.use(cookieParser())
+app.use(cors({
+    origin: ORIGIN,
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+}))
 
 const PORT = process.env.PORT || 3000
 app.use("/api/v1", appRouter);
