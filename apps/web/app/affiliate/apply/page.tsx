d3a4dfc3f2ad8be84d5841bf0b331e926ff7d13f
Vedant <borodevedant4@gmail.com>
2026-02-25 14:15:54 +0530

    feat(affiliate): add affiliate page

---
+++ apps/web/app/affiliate/apply/page.tsx
@@ -0,0 +1,73 @@
+"use client"
+
+import { AffiliateService } from "../../../lib/services/affiliate.service"
+import { useEffect, useState } from "react"
+import { toast } from "sonner"
+import { useAffiliateStore } from "../../../store/affiliate.store"
+import { useRouter } from "next/dist/client/components/navigation"
+
+export default function ApplyAffiliatePage() {
+      const router = useRouter()
+
+  const [loading, setLoading] = useState(false)
+    const { profile, fetchAffiliate } = useAffiliateStore()
+  useEffect(() => {
+    if (!profile) fetchAffiliate()
+    if (profile?.status === "APPROVED") {
+      router.replace("/affiliate")
+    }
+  }, [profile, fetchAffiliate, router])
+
+  const handleApply = async () => {
+    try {
+      setLoading(true)
+      await AffiliateService.apply({})
+      toast.success("Application submitted 🚀")
+    } catch (e: any) {
+      toast.error(e?.response?.data?.message)
+    } finally {
+      setLoading(false)
+    }
+  }
+
+  return (
+    <div className="max-w-3xl mx-auto py-16 px-4 space-y-10">
+
+      <div className="text-center space-y-3">
+        <h1 className="text-3xl font-bold text-primary">
+          Become a Little Stepz Affiliate
+        </h1>
+
+        <p className="text-muted">
+          Earn commission for every sale you refer.
+        </p>
+      </div>
+
+      {/* BENEFITS */}
+
+      <div className="grid md:grid-cols-3 gap-6 text-center">
+        <div className="border rounded-xl p-5">
+          <p className="font-semibold">High Commission</p>
+          <p className="text-sm text-muted">On every order</p>
+        </div>
+
+        <div className="border rounded-xl p-5">
+          <p className="font-semibold">Real-time Tracking</p>
+          <p className="text-sm text-muted">Clicks & conversions</p>
+        </div>
+
+        <div className="border rounded-xl p-5">
+          <p className="font-semibold">Monthly Payouts</p>
+          <p className="text-sm text-muted">Direct to your account</p>
+        </div>
+      </div>
+
+      <button
+        onClick={handleApply}
+        className="bg-primary text-white px-6 py-3 rounded-xl align-middle block mx-auto disabled:opacity-60"
+      >
+        {loading ? "Submitting…" : "Apply Now"}
+      </button>
+    </div>
+  )
+}
