4ce17ed3aa7f860b31d40bee528ba6f42d24850d
Vedant <borodevedant4@gmail.com>
2026-03-02 23:59:29 +0530

    refactor(config): add cloudinary for images to next.js config

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,7 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     transpilePackages: ["@repo/ui"],
-
+    images: {
+      remotePatterns: [
+        {
+          protocol: "https",
+          hostname: "res.cloudinary.com",
+        },
+      ],
+    },
 };
 
 export default nextConfig;
